decf7d9959f0de570ddad4927adbb59110f476c4
MateuszKurek98 <120757370+MateuszKurek98@users.noreply.github.com>
2023-10-11 20:40:24 +0200

    1. deleted test from pom.xml 2. added first endpoint ("/songs").

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -12,7 +12,6 @@ public class SongsController {
     public ResponseEntity<SongResponseDto> getAllSongs(){
         SongResponseDto response = new SongResponseDto(List.of("shawnmendes song1", "ariana grande song2"));
         return ResponseEntity.ok(response);
-
     }
 
 }
