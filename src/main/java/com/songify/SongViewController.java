54d055955503c301822d28a07866381e1e33135b
MateuszKurek98 <120757370+MateuszKurek98@users.noreply.github.com>
2023-11-03 23:06:23 +0100

    dodanie sekcji widoku .html

---
+++ src/main/java/com/songify/SongViewController.java
@@ -0,0 +1,27 @@
+package com.songify;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class SongViewController {
+    private Map<Integer, String> database = new HashMap<>();
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+    @GetMapping("/view/songs")
+    public String songs(Model model){
+        database.put(1, "shawnmendes song1");
+        database.put(2, "ariana grande song2");
+        database.put(3, "trzecia piosenka");
+        database.put(4, "czwarta piosenka");
+
+        model.addAttribute("songMap", database);
+        return "songs";
+    }
+}
